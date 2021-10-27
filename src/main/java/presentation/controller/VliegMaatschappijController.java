8f90b39c3fce1cb8c408382c9647f0d65958f77a
Mertcan417 <boymerti@gmail.com>
2021-10-27 23:17:01 +0200

    Made a start with controller

---
+++ src/main/java/presentation/controller/VliegMaatschappijController.java
@@ -0,0 +1,16 @@
+//package presentation;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping(path = "flightcompany")
+//public class VliegMaatschappijController {
+
+    //private Service service
+
+    //METHODS MAPPING
+    //Use service
+    //Do action with service method
+    //Throw Exceptions domain entity
+//}
