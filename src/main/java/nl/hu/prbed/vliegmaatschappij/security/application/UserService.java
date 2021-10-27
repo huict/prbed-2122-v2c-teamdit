1e8816d0d1fad94f7c2cc00192b470ebf4ee2f85
Sam Course <notcourseps@gmail.com>
2021-10-27 21:05:23 +0200

    Added Spring security
    Added standard Spring security functionality

---
+++ src/main/java/nl/hu/prbed/vliegmaatschappij/security/application/UserService.java
@@ -0,0 +1,36 @@
+package nl.hu.prbed.vliegmaatschappij.security.application;
+
+import nl.hu.prbed.vliegmaatschappij.security.data.UserRepository;
+import nl.hu.prbed.vliegmaatschappij.security.domain.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
+        this.userRepository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register(String username, String password, String firstName, String lastName) {
+        String encodedPassword = this.passwordEncoder.encode(password);
+
+        User user = new User(username, encodedPassword, firstName, lastName);
+
+        this.userRepository.save(user);
+    }
+
+    @Override
+    public User loadUserByUsername(String username) {
+        return this.userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
---
+++ src/main/java/nl/hu/prbed/vliegmaatschappij/security/presentation/dto/Login.java
@@ -0,0 +1,6 @@
+package nl.hu.prbed.vliegmaatschappij.security.presentation.dto;
+
+public class Login {
+    public String username;
+    public String password;
+}
