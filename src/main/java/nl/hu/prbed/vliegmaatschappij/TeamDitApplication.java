e645c6a9dd92270e5509f3efe5e5f278b30cacc9
Sam Course <notcourseps@gmail.com>
2021-10-27 19:02:32 +0200

    Initial commit

---
+++ src/main/java/nl/hu/prbed/vliegmaatschappij/TeamDitApplication.java
@@ -0,0 +1,13 @@
+package nl.hu.prbed.vliegmaatschappij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/hu/prbed/vliegmaatschappij/TeamDitApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hu.prbed.vliegmaatschappij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
