45a8550d4a6e586fff0cac1f613b998dc4a6dd59
Jasper <jasper.de.bie1998@gmail.com>
2021-10-28 12:00:15 +0200

    Airport
    Airport Domain Entity
    Airport DTO

---
+++ src/main/java/nl/hu/prbed/vliegmaatschappij/controller/DTO/Airport.java
@@ -0,0 +1,10 @@
+package nl.hu.prbed.vliegmaatschappij.controller.DTO;
+
+public class Airport {
+    public String naam;
+    public String stad;
+    public String code;
+    public double longitude;
+    public double latitude;
+
+}
