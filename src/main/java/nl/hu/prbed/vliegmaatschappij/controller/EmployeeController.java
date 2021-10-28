38ce517ff7a6f1eb2abd38c0107324568079c70f
Mertcan417 <boymerti@gmail.com>
2021-10-28 14:39:55 +0200

    changed Employeecontroller to readable class

--- src/main/java/nl/hu/prbed/vliegmaatschappij/controller/EmployeeController.java
+++ src/main/java/nl/hu/prbed/vliegmaatschappij/controller/EmployeeController.java
@@ -27,7 +27,10 @@ public class EmployeeController {
 
     //zoeken van vliegroute
     @GetMapping("/vliegroute")
-    public ResponseEntity<Collection<Employee>> flightrouteView() {
+    public void flightrouteView() {
+    }
+
+    private ResponseEntity<Collection<Employee>> getCollectionResponseEntity() {
         Collection<Employee> employees = new ArrayList<>();
         List<Employee> response = new ArrayList<>();
 
@@ -44,377 +47,113 @@ public class EmployeeController {
 
     //vliegroute toevoegen
     @PostMapping("/vliegroute")
-    public ResponseEntity<Collection<Employee>> flightrouteAdd() {
-        Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void flightrouteAdd() {
     }
 
     //vliegroute aanpassen
     @PostMapping("/vliegroute")
-    public ResponseEntity<Collection<Employee>> flightrouteEdit() {
-        Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void flightrouteEdit() {
 
     }
 
     //zoeken van luchthavens
     @GetMapping("/luchthaven/inzien")
-    public ResponseEntity<Collection<Employee>> airportView() {
-        Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void airportView() {
     }
 
     //toevoegen van luchthaven
     @PostMapping("/luchthaven/toevoegen")
-    public ResponseEntity<Collection<Employee>> airportAdd() {
-        Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void airportAdd() {
     }
 
     //aanpassen van luchthaven
     @PostMapping("/luchthaven/aanpassen")
-    public ResponseEntity<Collection<Employee>> airportEdit() {
-      Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void airportEdit() {
 
     }
 
     //zoeken van vliegtuig op basis van id
     @GetMapping("/vliegtuig")
-    public ResponseEntity<Collection<Employee>> planeView() {
-        Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void planeView() {
     }
 
     //toevoegen van vliegtuig
     @PostMapping("/vliegtuig")
-    public ResponseEntity<Collection<Employee>> planeAdd() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void planeAdd() {
     }
 
     //aanpassen van vliegtuig op basis van id
     @PostMapping("/vliegtuig/aanpassen")
-    public ResponseEntity<Collection<Employee>> planeEdit() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void planeEdit() {
     }
 
     //zoeken van alle vluchten
     @GetMapping("/vlucht")
-    public ResponseEntity<Collection<Employee>> flightView() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void flightView() {
     }
 
     //toevoegen van vlucht
     @PostMapping("/vlucht")
-    public ResponseEntity<Collection<Employee>> flightAdd() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void flightAdd() {
     }
 
     //aanpassen van vlucht op basis van id/naam
     @PostMapping("/vlucht")
-    public ResponseEntity<Collection<Employee>> flightEdit() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void flightEdit() {
     }
 
     //annuleren van vlucht op basis van id
     @PostMapping("/vlucht")
-    public ResponseEntity<Collection<Employee>> flightCancel() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-//            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void flightCancel() {
     }
 
     //zoeken van boekingen
     @GetMapping("/boeking")
-    public ResponseEntity<Collection<Employee>> bookingView() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void bookingView() {
     }
 
     //toevoegen van boeking
     @PostMapping("/boeking")
-    public ResponseEntity<Collection<Employee>> bookingAdd() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void bookingAdd() {
     }
 
     //aanpassen van boeking op basis van id
     @PutMapping("/boeking")
-    public ResponseEntity<Collection<Employee>> bookingEdit() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void bookingEdit() {
     }
 
     //zoeken van alle klanten
     @GetMapping("/klant")
-    public ResponseEntity<Collection<Employee>> customerView() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void customerView() {
     }
 
     //toevoegen van klant
     @PostMapping("/klant")
-    public ResponseEntity<Collection<Employee>> customerAdd() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void customerAdd() {
     }
 
     //aanpassen van klant op basis van id
     @PutMapping("/klant")
-    public ResponseEntity<Collection<Employee>> customerEdit() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void customerEdit() {
     }
 
     //zoeken van medewerkers
     @GetMapping("/medewerker")
-    public ResponseEntity<Collection<Employee>> employeeView() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void employeeView() {
     }
 
     //toevoegen van medewerker
     @PostMapping("/medewerker")
-    public ResponseEntity<Collection<Employee>> employeeAdd() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void employeeAdd() {
     }
 
     //aanpassen van medewerker
     @PostMapping("/medewerker")
-    public ResponseEntity<Collection<Employee>> employeeEdit() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void employeeEdit() {
     }
 
     //verwijderen van medewerker
     @DeleteMapping("/medewerker")
-    public ResponseEntity<Collection<Employee>> employeeDelete() {
-          Collection<Employee> employees = new ArrayList<>();
-        List<Employee> response = new ArrayList<>();
-
-        employees.forEach(employee -> {
-            if (employees.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-//            employee.add(linkTo(methodOn(EmployeeController.class).findByid(employee.getId())).withSelfRel());
-            response.add(employee);
-        });
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public void employeeDelete() {
     }
 }
