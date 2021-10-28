38fac03ea1b67c38385e1e536087e8ae5de77e5c
Mertcan417 <boymerti@gmail.com>
2021-10-28 14:18:08 +0200

    changed Employee class and add a link for response

--- src/main/java/nl/hu/prbed/vliegmaatschappij/domain/Employee.java
+++ src/main/java/nl/hu/prbed/vliegmaatschappij/domain/Employee.java
@@ -1,23 +1,51 @@
 package nl.hu.prbed.vliegmaatschappij.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
-public class Employee {
+
+@Component
+@Entity
+public class Employee extends RepresentationModel<Employee> {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
 
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
 
-    public Employee(String firstName, String lastName, Date dateOfBirth){
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @JsonCreator
+    public Employee(@JsonProperty("id") Long id, @JsonProperty("firstname") String firstName, @JsonProperty("lastname") String lastName, @JsonProperty("birthday") Date dateOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
     }
 
-//    maybe function instead permissionlvl?
-    enum permissionLevel{
+    enum permissionLevel {
         LOW,
         MEDIUM,
         HIGH
     }
+
 }
