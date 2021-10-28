b962b664d65abc62275bd363a46122cc36a45fa5
Mertcan417 <boymerti@gmail.com>
2021-10-28 13:48:08 +0200

    add a repository for the flightcompany

--- src/main/java/nl/hu/prbed/vliegmaatschappij/data/FlightCompanyRepository.java
+++ src/main/java/nl/hu/prbed/vliegmaatschappij/data/FlightCompanyRepository.java
@@ -1,6 +1,15 @@
 package nl.hu.prbed.vliegmaatschappij.data;
 
 
-public interface FlightCompanyRepository {
+import nl.hu.prbed.vliegmaatschappij.application.FlightCompanyService;
+import nl.hu.prbed.vliegmaatschappij.domain.Airport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FlightCompanyRepository extends JpaRepository<Airport,Long> {
+
+
+    Optional<Airport> findAllByAirportName(String name);
 
 }
