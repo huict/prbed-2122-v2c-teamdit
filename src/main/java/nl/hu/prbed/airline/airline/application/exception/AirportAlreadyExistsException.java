3c0c341fa99f9174ed31fd770e49a30a6c1938cc
Sam Course <notcourseps@gmail.com>
2021-10-31 17:19:24 +0100

    Proper exception handling
    Remove stack traces from exceptions returned from endpoints
    Added custom exceptions for 404 and 409
    Removed old unnecessary code

---
+++ src/main/java/nl/hu/prbed/airline/airline/application/exception/AirportAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package nl.hu.prbed.airline.airline.application.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT, reason="Duplicate airport code")
+public class AirportAlreadyExistsException extends RuntimeException {
+    public AirportAlreadyExistsException(String code) {
+        super("Airport already exists with code: " + code);
+    }
+}
--- src/main/java/nl/hu/prbed/airline/security/presentation/controller/RegistrationController.java
+++ src/main/java/nl/hu/prbed/airline/security/presentation/controller/RegistrationController.java
@@ -2,9 +2,6 @@ package nl.hu.prbed.airline.security.presentation.controller;
 
 import nl.hu.prbed.airline.security.application.UserService;
 import nl.hu.prbed.airline.security.presentation.dto.Registration;
-import nl.hu.prbed.airline.security.domain.exception.UserNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,24 +23,4 @@ public class RegistrationController {
                 registration.lastName
         );
     }
-
-
-
-    // Temporary methods for exception handler testing
-
-    @GetMapping("/error")
-    public void error() {
-        throw new UserNotFoundException("User not found");
-    }
-
-    @ExceptionHandler(UserNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<String> handleNoSuchElementFoundException(
-            UserNotFoundException exception
-    ) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
-    }
-
 }
