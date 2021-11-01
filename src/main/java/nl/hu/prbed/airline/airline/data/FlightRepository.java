04c5440f28f17c7507ed75ed5bfa322e44dba4ba
Mertcan417 <boymerti@gmail.com>
2021-11-01 18:44:33 +0100

    Add a flight repository
    I add a flight repository and made some little syntax changes on the other repositories

---
+++ src/main/java/nl/hu/prbed/airline/airline/data/FlightRepository.java
@@ -0,0 +1,19 @@
+package nl.hu.prbed.airline.airline.data;
+
+import nl.hu.prbed.airline.airline.domain.Flight;
+import nl.hu.prbed.airline.airline.domain.FlightRoute;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight,Long> {
+    Optional<Flight> findByRouteAndDepartureTime(FlightRoute route, LocalDateTime departureTime);
+    List<Flight> findAllByDepartureTime(LocalDateTime departure);
+    void deleteByDepartureTimeAndRoute(LocalDateTime departureTime,FlightRoute route);
+    void deleteById(Long id);
+
+}
