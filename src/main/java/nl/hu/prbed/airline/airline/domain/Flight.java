b1e3335ef06988755de8c67c6ab69c0fdc7f0789
Mertcan417 <boymerti@gmail.com>
2021-11-01 18:57:57 +0100

    Changed class Flight
    Add some new methods in the flight class

--- src/main/java/nl/hu/prbed/airline/airline/domain/Flight.java
+++ src/main/java/nl/hu/prbed/airline/airline/domain/Flight.java
@@ -1,6 +1,7 @@
 package nl.hu.prbed.airline.airline.domain;
 
 
+import nl.hu.prbed.airline.airline.domain.user.Customer;
 import org.hibernate.annotations.Cascade;
 import org.springframework.stereotype.Component;
 import org.hibernate.annotations.CascadeType;
@@ -10,6 +11,7 @@ import java.sql.Time;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Entity
@@ -39,10 +41,71 @@ public class Flight {
 
     }
 
-    public Flight(LocalDateTime  departureTime, FlightRoute flightRoute, Plane plane){
+    public Flight(LocalDateTime departureTime, FlightRoute flightRoute, Plane plane) {
         this.departureTime = departureTime;
         this.route = flightRoute;
         this.plane = plane;
     }
 
+    public void addBooking(Booking booking) {
+        if (booking.getBookingClass().equals(BookingClass.BUSINESS) && bookings.size() < plane.getSeatsBusiness()) {
+            bookings.add(booking);
+        } else if (booking.getBookingClass().equals(BookingClass.ECONOMY) && bookings.size() < plane.getSeatsEconomy()) {
+            bookings.add(booking);
+        } else if (booking.getBookingClass().equals(BookingClass.FIRST) && bookings.size() < plane.getSeatsFirstClass()) {
+            bookings.add(booking);
+        }
+    }
+
+    public int flightOccupation() {
+        return bookings.size();
+    }
+
+    public int seatsAvailable() {
+        int totalSeats = plane.getSeatsFirstClass() + plane.getSeatsEconomy() + plane.getSeatsBusiness();
+        return bookings.size() < totalSeats ? totalSeats - bookings.size() : 0;
+    }
+
+
+    public List<Booking> customersForBussinesClass() {
+        return bookings.stream().filter(booking ->
+                        booking.getBookingClass().equals(BookingClass.BUSINESS))
+                .collect(Collectors.toList());
+    }
+
+
+    public List<Booking> customersForEconomyClass() {
+        return bookings.stream().filter(booking ->
+                        booking.getBookingClass().equals(BookingClass.BUSINESS))
+                .collect(Collectors.toList());
+    }
+
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+
+    public FlightRoute getRoute() {
+        return route;
+    }
+
+    public void setRoute(FlightRoute route) {
+        this.route = route;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
