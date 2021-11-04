98d161e04e935658eb2dd8fa11e4a0972ad8ad97
Jasper <jasper.de.bie1998@gmail.com>
2021-11-04 12:07:22 +0100

    Lombok Flight + Date -> LocalDateTime
    Added Lombok functionality and also refactored everything to use LocalDateTime instead of Date (LocalDateTime offers more functionality)

--- src/main/java/nl/hu/prbed/airline/airline/data/FlightRepository.java
+++ src/main/java/nl/hu/prbed/airline/airline/data/FlightRepository.java
@@ -6,14 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight,Long> {
     Optional<Flight> findByRouteAndDepartureTime(FlightRoute route, Date departureTime);
-    List<Flight> findAllByDepartureTime(Date departure);
-    void deleteByDepartureTimeAndRoute(Date departureTime,FlightRoute route);
+    List<Flight> findAllByDepartureTime(LocalDateTime departure);
+    void deleteByDepartureTimeAndRoute(LocalDateTime departureTime,FlightRoute route);
     void deleteFlightById(Long id);
 
 }
