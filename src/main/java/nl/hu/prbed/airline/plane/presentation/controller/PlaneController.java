992e5a108666108998cf76ee8d5159f44fe5e4a5
Mertcan417 <boymerti@gmail.com>
2021-11-05 01:38:49 +0100

    add requestmapping to PlaneController
    There wasnt a requestmapping before, this could also cause the whitelabel error page.

--- src/main/java/nl/hu/prbed/airline/plane/presentation/controller/PlaneController.java
+++ src/main/java/nl/hu/prbed/airline/plane/presentation/controller/PlaneController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/plane")
 public class PlaneController {
 
     private final PlaneService planeService;
