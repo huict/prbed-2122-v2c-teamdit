f3d671f459b6875a538ff2b34ab503a49273e05b
Mertcan417 <boymerti@gmail.com>
2021-11-05 17:33:25 +0100

    add resource path to make fleet
    I add a new path to make a fleet via the airlinecontroller

--- src/main/java/nl/hu/prbed/airline/airline/presentation/controller/AirlineController.java
+++ src/main/java/nl/hu/prbed/airline/airline/presentation/controller/AirlineController.java
@@ -1,6 +1,7 @@
 package nl.hu.prbed.airline.airline.presentation.controller;
 
 import nl.hu.prbed.airline.airline.application.AirlineService;
+import nl.hu.prbed.airline.fleet.domain.Fleet;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +12,11 @@ public class AirlineController {
 
     public AirlineController(AirlineService airlineService) {
         this.airlineService = airlineService;
+    }
+
+    @PostMapping(path = "/fleet")
+    public Fleet addFleet(){
+        return airlineService.addFleet();
     }
 
 }
