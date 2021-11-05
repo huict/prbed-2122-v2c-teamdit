206f2d07e882e63e1eb5dead211876c7ad93e2df
Mertcan417 <boymerti@gmail.com>
2021-11-05 17:32:51 +0100

    add fleet repository to airline service
    It is only possible to make a fleet with the airlineservice, so we need to save that with the fleetrepository.

--- src/main/java/nl/hu/prbed/airline/airline/application/AirlineService.java
+++ src/main/java/nl/hu/prbed/airline/airline/application/AirlineService.java
@@ -1,17 +1,24 @@
 package nl.hu.prbed.airline.airline.application;
 
+import nl.hu.prbed.airline.airline.application.exception.MoreThanOneFleetException;
 import nl.hu.prbed.airline.airline.application.exception.NoAirlineExistsException;
 import nl.hu.prbed.airline.airline.data.AirlineRepository;
 import nl.hu.prbed.airline.airline.domain.Airline;
+import nl.hu.prbed.airline.fleet.data.FleetRepository;
+import nl.hu.prbed.airline.fleet.domain.Fleet;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class AirlineService {
 
     private final AirlineRepository airlineRepository;
+    private final FleetRepository fleetRepository;
 
-    public AirlineService(AirlineRepository airlineRepository){
+    public AirlineService(AirlineRepository airlineRepository, FleetRepository fleetRepository){
         this.airlineRepository = airlineRepository;
+        this.fleetRepository = fleetRepository;
     }
 
     public Airline getAirline(){
@@ -22,4 +29,14 @@ public class AirlineService {
             throw new NoAirlineExistsException();
         }
     }
+
+    public Fleet addFleet(){
+        if(fleetRepository.findAll().isEmpty()) {
+            Fleet newFleet = new Fleet(new ArrayList<>());
+            fleetRepository.save(newFleet);
+            return newFleet;
+        }
+        throw new MoreThanOneFleetException("There is already a fleet defined!");
+    }
+
 }
