b69bcd0789bc42c074cb05e24b10279677351623
Jasper <jasper.de.bie1998@gmail.com>
2021-11-05 14:31:29 +0100

    error fix

--- src/main/java/nl/hu/prbed/airline/flightroute/data/FlightRouteRepository.java
+++ src/main/java/nl/hu/prbed/airline/flightroute/data/FlightRouteRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public interface FlightRouteRepository extends JpaRepository<FlightRoute,Long> {
     List<FlightRoute> findAll();
-    boolean existsById();
+    boolean existsById(Long id);
     boolean existsByDepartureLocationAndArrivalLocationAndDurationMinutesAndPriceEconomyAndPriceBusinessAndPriceFirstClass(String departure, String arrival, int minutes, double economy, double business, double firstClass);
     Optional<FlightRoute> findByDepartureLocationAndArrivalLocationAndDurationMinutesAndPriceEconomyAndPriceBusinessAndPriceFirstClass(String departure, String arrival, int minutes, double economy, double business, double firstClass);
 }
