a44c4e5895af18d1fb21173c5a3845541a2741b6
Mertcan417 <boymerti@gmail.com>
2021-11-06 02:27:24 +0100

    Add NoSeatsException
    If there aren't enough seats this exception will be thrown

---
+++ src/main/java/nl/hu/prbed/airline/booking/application/exception/NoSeatsLeftForClassException.java
@@ -0,0 +1,12 @@
+package nl.hu.prbed.airline.booking.application.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "There are no seats available for the given class!")
+public class NoSeatsLeftForClassException extends RuntimeException{
+    public NoSeatsLeftForClassException() {
+        super("Seats for given class are unavailable!");
+    }
+
+}
