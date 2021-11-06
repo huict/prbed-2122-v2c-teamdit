a9ded010b0395da9ad88bd57abf866ed90d2ef8e
Mertcan417 <boymerti@gmail.com>
2021-11-06 02:30:01 +0100

    Made a manual delete query
    I had an issue with using the standard deleteById method, so I made a delete method by myself.

--- src/main/java/nl/hu/prbed/airline/flight/data/FlightRepository.java
+++ src/main/java/nl/hu/prbed/airline/flight/data/FlightRepository.java
@@ -3,18 +3,24 @@ package nl.hu.prbed.airline.flight.data;
 import nl.hu.prbed.airline.flight.domain.Flight;
 import nl.hu.prbed.airline.flightroute.domain.FlightRoute;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.sql.Date;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface FlightRepository extends JpaRepository<Flight,Long> {
     Optional<Flight> findByRouteAndDepartureTime(FlightRoute route, LocalDateTime departureTime);
     List<Flight> findAllByDepartureTime(LocalDateTime departure);
-    void deleteByDepartureTimeAndRoute(LocalDateTime departureTime,FlightRoute route);
-    void deleteFlightById(Long id);
+
+    @Modifying
+    @Query(value = "DELETE FROM Flight f WHERE f.id = ?1",nativeQuery = true)
+    void deleteById(Long id);
 
 }
