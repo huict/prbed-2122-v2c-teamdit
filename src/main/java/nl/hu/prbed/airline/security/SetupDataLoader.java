77ea40450da44edabcf29fb8ede5875571faf676
Course <notcourseps@gmail.com>
2021-11-07 01:04:39 +0100

    Create single airport and fleet on application initialization

--- src/main/java/nl/hu/prbed/airline/security/SetupDataLoader.java
+++ src/main/java/nl/hu/prbed/airline/security/SetupDataLoader.java
@@ -1,5 +1,9 @@
 package nl.hu.prbed.airline.security;
 
+import nl.hu.prbed.airline.airline.data.AirlineRepository;
+import nl.hu.prbed.airline.airline.domain.Airline;
+import nl.hu.prbed.airline.fleet.data.FleetRepository;
+import nl.hu.prbed.airline.fleet.domain.Fleet;
 import nl.hu.prbed.airline.security.data.RoleRepository;
 import nl.hu.prbed.airline.security.data.UserRepository;
 import nl.hu.prbed.airline.security.domain.Role;
@@ -11,6 +15,8 @@ import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
+
 import static nl.hu.prbed.airline.security.domain.Role.getAllRoleNames;
 
 @Component
@@ -21,13 +27,19 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final AirlineRepository airlineRepository;
+    private final FleetRepository fleetRepository;
 
-    public SetupDataLoader(RoleRepository roleRepository, PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    public SetupDataLoader(RoleRepository roleRepository, PasswordEncoder passwordEncoder, UserRepository userRepository, AirlineRepository airlineRepository, FleetRepository fleetRepository) {
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
+        this.airlineRepository = airlineRepository;
+        this.fleetRepository = fleetRepository;
     }
 
+    // Temporary replacement for this being a non-production environment
+    // and there being no better alternative for adding these data entries
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         if (alreadySetup) {
@@ -38,20 +50,26 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
             createRoleIfNotFound(roleName);
         }
 
-        Role adminRole = roleRepository.findByName("ROLE_ADMIN");
-
         if (!userRepository.existsByUsername("admin")) {
+            Role adminRole = roleRepository.findByName("ROLE_ADMIN");
             User user = new User("admin", passwordEncoder.encode("admin"), "Ad", "Min");
             user.addRole(adminRole);
             userRepository.save(user);
         }
 
-        Role employeeRole = roleRepository.findByName("ROLE_EMPLOYEE");
 
         if (!userRepository.existsByUsername("employee")) {
+            Role employeeRole = roleRepository.findByName("ROLE_EMPLOYEE");
             User user = new User("employee", passwordEncoder.encode("employee"), "emplo", "yee");
             user.addRole(employeeRole);
             userRepository.save(user);
+        }
+
+        if (airlineRepository.findAll().size() != 1) {
+            Fleet fleet = new Fleet();
+            Airline airline = new Airline(fleet, new ArrayList<>(), new ArrayList<>());
+
+            airlineRepository.save(airline);
         }
 
         alreadySetup = true;
