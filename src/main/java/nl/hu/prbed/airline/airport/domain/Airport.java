9c35bd42e7fe1cbe37b0a2a2ae56d776794fa4e0
Jasper <jasper.de.bie1998@gmail.com>
2021-11-10 17:54:30 +0100

    Airport constructor update
    make sure ICAO code is capitalized

--- src/main/java/nl/hu/prbed/airline/airport/domain/Airport.java
+++ src/main/java/nl/hu/prbed/airline/airport/domain/Airport.java
@@ -6,11 +6,11 @@ import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
+import java.util.Locale;
 
 @Entity
 @Component
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 public class Airport {
     @Id
@@ -21,4 +21,13 @@ public class Airport {
     private String country;
     private double longitude;
     private double latitude;
+
+    public Airport(String codeICAO, String airportName, String city, String country, double longitude, double latitude) {
+        this.codeICAO = codeICAO.toUpperCase(Locale.ROOT);
+        this.airportName = airportName;
+        this.city = city;
+        this.country = country;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
 }
