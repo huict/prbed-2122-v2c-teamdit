271e808fa23b4c30cf3ee0988aa0e07bc3ff1fdd
Sam Course <notcourseps@gmail.com>
2021-11-14 20:47:51 +0100

    Fixed PlaneController not throwing HTTP exception

--- src/main/java/nl/hu/prbed/airline/plane/presentation/controller/PlaneController.java
+++ src/main/java/nl/hu/prbed/airline/plane/presentation/controller/PlaneController.java
@@ -9,6 +9,7 @@ import nl.hu.prbed.airline.plane.domain.Plane;
 import nl.hu.prbed.airline.plane.presentation.dto.PlaneRequestDTO;
 import nl.hu.prbed.airline.plane.presentation.dto.PlaneResponseDTO;
 import nl.hu.prbed.airline.plane.presentation.exception.InvalidDTOHTTPException;
+import nl.hu.prbed.airline.plane.presentation.exception.PlaneInUseHTTPException;
 import nl.hu.prbed.airline.plane.presentation.exception.PlaneNotFoundHTTPException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -45,8 +46,7 @@ public class PlaneController {
         try {
             Plane plane = this.planeService.getPlaneById(id);
             return new PlaneResponseDTO(plane);
-        }
-        catch (PlaneNotFoundException e){
+        } catch (PlaneNotFoundException e) {
             throw new PlaneNotFoundHTTPException(id);
         }
     }
@@ -55,8 +55,7 @@ public class PlaneController {
     public PlaneResponseDTO updatePlane(@RequestBody PlaneRequestDTO planeRequestDTO) {
         try {
             return this.planeService.updatePlane(planeRequestDTO);
-        }
-        catch (PlaneNotFoundException e){
+        } catch (PlaneNotFoundException e) {
             throw new PlaneNotFoundHTTPException(planeRequestDTO.id);
         }
     }
@@ -65,12 +64,10 @@ public class PlaneController {
     public Boolean deletePlane(@PathVariable Long id) {
         try {
             return this.planeService.deletePlane(id);
-        }
-        catch (PlaneNotFoundException e){
+        } catch (PlaneNotFoundException e) {
             throw new PlaneNotFoundHTTPException(id);
-        }
-        catch  (DataIntegrityViolationException e) {
-            throw new PlaneInUseException("Plane is still in use");
+        } catch (DataIntegrityViolationException e) {
+            throw new PlaneInUseHTTPException("Plane is still in use");
         }
     }
 }
